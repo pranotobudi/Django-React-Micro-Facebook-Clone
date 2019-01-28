65c80a3784ab0f7e5a9fda199c5668d205f7f965
pranotobudi <pranotobudi.s@gmail.com>
2019-01-29 05:09:13 +0700

    add MicroFbPostComment's item

--- frontend/micro_facebook_frontend/src/MicroFbPostComment.js
+++ frontend/micro_facebook_frontend/src/MicroFbPostComment.js
@@ -1,17 +1,32 @@
 /*eslint-disable*/
 import React from 'react';
+import { Avatar } from 'antd'
 import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 
 const MicroFbPostComment = () => (
     <div>
-        <div>
-            Comment Avatar
+        <div className="micro-fb-inline-block micro-fb-post-comment-avatar-wrapper">
+            <div className="micro-fb-inline-block micro-fb-post-comment-avatar">
+                <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />
+            </div>
         </div>
-        <div>
-            Post Comment Text
-        </div>
-        <div>
-            Post Comment Button
+        <div className="micro-fb-inline-block micro-fb-post-comment-second-wrapper">
+            <div>
+                <div className="micro-fb-inline-block micro-fb-post-comment-name">
+                    Firstname Lastname
+                </div>
+                <div className="micro-fb-inline-block micro-fb-post-comment-text">
+                    Comment reply Comment reply Comment reply Comment reply ... ....
+                </div>
+            </div>
+            <div>
+                <div className="micro-fb-inline-block micro-fb-post-comment-like-button">
+                    Like
+                </div>
+                <div className="micro-fb-inline-block micro-fb-post-comment-reply-button">
+                    Reply
+                </div>
+            </div>
         </div>
 
     </div>
