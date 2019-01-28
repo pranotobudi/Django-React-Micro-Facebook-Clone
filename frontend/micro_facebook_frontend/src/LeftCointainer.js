f875f4a2018673ddc8c8bed4bdce0ac4ccb3ffe4
pranotobudi <pranotobudi.s@gmail.com>
2019-01-28 12:45:32 +0700

    create MicroFBCard component

--- frontend/micro_facebook_frontend/src/LeftCointainer.js
+++ frontend/micro_facebook_frontend/src/LeftCointainer.js
@@ -1,17 +1,20 @@
+/* eslint-disable */
 import React from 'react';
 import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 import { Card } from 'antd';
+import MicroFbCard from './MicroFbCard';
 
 const LeftContainer = () => (
     <Card
-        title="Default size card"
+        title="My Groups Shortcuts"
         extra={<a href="http://localhost">More</a>}
         style={{ width: 300 }}>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
+        <p>Django Python Web Framework</p>
+        <p>React Developers</p>
+        <p>Python Wordwide Community</p>
     </Card>
-
+    // <MicroFbCard />
       );
 
 export default LeftContainer;
+/* eslint-enable */
