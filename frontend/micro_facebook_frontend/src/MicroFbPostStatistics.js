b34501312cdec05c1e83e87b4a093e20272f3355
pranotobudi <pranotobudi.s@gmail.com>
2019-01-28 17:07:22 +0700

    Add MicroFbPostStatistics component's Items

--- frontend/micro_facebook_frontend/src/MicroFbPostStatistics.js
+++ frontend/micro_facebook_frontend/src/MicroFbPostStatistics.js
@@ -1,11 +1,18 @@
 /*eslint-disable*/
 import React from 'react';
 import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
-
+import {Icon} from 'antd'
 const MicroFbPostStatistics = () => (
     <div>
-        <div>
-            Post Statistics
+        <div className="micro-fb-inline-block micro-fb-post-statistics-left">
+            <div className="micro-fb-inline-block"><Icon type="like" /></div>
+            <div className="micro-fb-inline-block micro-fb-fb-post-statistics-total-like">45</div>
+        </div>
+        <div className="micro-fb-inline-block micro-fb-post-statistics-right">
+            <div className="micro-fb-inline-block micro-fb-fb-post-statistics-total-comment">15</div>
+            <div className="micro-fb-inline-block micro-fb-fb-post-statistics-comment-text">Comments</div>
+            <div className="micro-fb-inline-block micro-fb-fb-post-statistics-total-share">5</div>
+            <div className="micro-fb-inline-block micro-fb-fb-post-statistics-share-text">Shares</div>
         </div>
     </div>
       );
