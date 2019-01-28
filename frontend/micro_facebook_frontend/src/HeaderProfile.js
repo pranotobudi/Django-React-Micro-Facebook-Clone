15cc3793dddff4739dd4f4b888c32b205828e9d8
pranotobudi <pranotobudi.s@gmail.com>
2019-01-28 10:15:22 +0700

    adjust Components position in the header

--- frontend/micro_facebook_frontend/src/HeaderProfile.js
+++ frontend/micro_facebook_frontend/src/HeaderProfile.js
@@ -1,13 +1,15 @@
+/* eslint-disable */
 import React from 'react';
 import { Avatar } from 'antd';
 import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 import './App.css';
 
 const HeaderProfile = () => (
-    <div className="header-profile-text">
+    <div className="header-profile">
         <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />
         <span className="header-profile-text">FirstName</span>
     </div>
       );
 
 export default HeaderProfile;
+/* eslint-enable */
