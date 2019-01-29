eebe3c39c6a9f94c93c61c782387fe5b76084b9e
pranotobudi <pranotobudi.s@gmail.com>
2019-01-29 15:02:46 +0700

    add Home Menu component

--- frontend/micro_facebook_frontend/src/HomeMainContainer.js
+++ frontend/micro_facebook_frontend/src/HomeMainContainer.js
@@ -3,13 +3,16 @@ import React from 'react';
 import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 import './index.css';
 import HomeCoverPhoto from './HomeCoverPhoto';
+import HomeMenu from './HomeMenu';
 
 const HomeMainContainer = () => (
     <div>
         <div>
             <HomeCoverPhoto />
             </div>
-        <div>Home Menu</div>
+        <div>
+            <HomeMenu />
+        </div>
         <div>
             Home Body
             <div>Home Bio</div>
