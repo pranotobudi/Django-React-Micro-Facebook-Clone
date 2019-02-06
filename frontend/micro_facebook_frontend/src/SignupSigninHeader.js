fd82453e71fd5af3e790193b37d32c6939fca3b3
pranotobudi <pranotobudi.s@gmail.com>
2019-02-06 10:18:55 +0700

    add SignupSigninHeader component

--- frontend/micro_facebook_frontend/src/SignupSigninHeader.js
+++ frontend/micro_facebook_frontend/src/SignupSigninHeader.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Input, Button } from 'antd';
 import myImage from './facebook-text-white.jpg';
 
 const SignupSigninHeader = () => (
     <div className="signup-signin-header">
         <div className="signup-signin-header-brand">
-            <img src={myImage} width="300px" height="100px" alt="facebook-text-white" />
+            <img src={myImage} width="250px" height="80px" alt="facebook-text-white" />
         </div>
-        <div className="signup-signin-header-login">Right Section - Login</div>
+        <div className="signup-signin-header-login">
+            <div className="signup-signin-header-login-email">
+                <div className="signup-signin-header-login-email-text">Email</div>
+                <div>
+                    <Input />
+                </div>
+                <div>
+                    <input type="checkbox" />
+                    <span className="signup-signin-header-login-email-text-bottom">Keep Me Logged in</span>
+                </div>
+            </div>
+            <div className="signup-signin-header-login-password">
+                <div className="signup-signin-header-login-password-text">Password</div>
+                <div>
+                    <Input />
+                </div>
+                <div>
+                    <input type="checkbox" />
+                    <span className="signup-signin-header-login-password-text-bottom">Forget your password?</span>
+                </div>
+            </div>
+            <Button type="primary">Log in</Button>
+
+        </div>
     </div>
 );
 
