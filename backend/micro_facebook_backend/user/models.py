e28ae53c660a5d417312895a0d67d853b9ceb467
pranotobudi <pranotobudi.s@gmail.com>
2019-02-08 13:03:59 +0700

    backend: add User model

--- backend/micro_facebook_backend/user/models.py
+++ backend/micro_facebook_backend/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    firstName =  models.CharField(max_length=120)
+    lastName =  models.CharField(max_length=120)
+    email =  models.EmailField()
+    password =  models.CharField(max_length=120)
+    gender =  models.CharField(max_length=120)
+    birthday =  models.CharField(max_length=120)
